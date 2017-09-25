d22500d7581e25c29e3522cee47bc53f5f27d06a
DIWithers <d.ivy.withers@gmail.com>
2017-09-25 12:18:48 -0400

    We can now handle any missing barcode

--- src/Sale.js
+++ src/Sale.js
@@ -7,8 +7,8 @@ var Sale = (function () {
             this.display.setText("7.95");
         else if (barcode === "23456")
             this.display.setText("12.50");
-        else if (barcode === "99999")
-            this.display.setText("Product not found for 99999");
+        else
+            this.display.setText("Product not found for " + barcode);
     };
     return Sale;
 }());
--- src/Sale.ts
+++ src/Sale.ts
@@ -8,6 +8,6 @@ class Sale {
     onBarcode(barcode: string): void {
         if (barcode === "12345") this.display.setText("7.95");
         else if (barcode === "23456") this.display.setText("12.50");
-        else if (barcode === "99999") this.display.setText("Product not found for 99999");
+        else this.display.setText("Product not found for " + barcode);
     }
 }
