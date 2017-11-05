03ef3e97960bbb89889086ee55b90b6a2fe14487
DIWithers <d.ivy.withers@gmail.com>
2017-11-05 09:52:36 -0500

    Pushed the price list out to client as a parameter FIX

--- src/Sale.js
+++ src/Sale.js
@@ -1,20 +1,17 @@
 var Sale = (function () {
     function Sale(display, pricesByBarcode) {
         this.display = display;
-        //details in data
         this.pricesByBarcode = pricesByBarcode;
     }
-
     Sale.prototype.onBarcode = function (barcode) {
-        //abstraction in code
-        if (barcode === "")
+        if (barcode === "") {
             this.display.setText("Scanning error: empty barcode");
-        else {
-            if (this.pricesByBarcode.has(barcode))
-                this.display.setText(this.pricesByBarcode.get(barcode));
-            else
-                this.display.setText("Product not found for " + barcode);
+            return;
         }
+        if (this.pricesByBarcode.has(barcode))
+            this.display.setText(this.pricesByBarcode.get(barcode));
+        else
+            this.display.setText("Product not found for " + barcode);
     };
     return Sale;
 }());
--- src/Sale.ts
+++ src/Sale.ts
@@ -4,15 +4,14 @@ class Sale {
 
     constructor(display: Display, pricesByBarcode: any) {
         this.display = display;
-        //details in data
         this.pricesByBarcode = pricesByBarcode
     }
     onBarcode(barcode: string): void {
-        //abstraction in code
-        if (barcode === "") this.display.setText("Scanning error: empty barcode");
-        else {
+        if (barcode === "") {
+            this.display.setText("Scanning error: empty barcode");
+            return;
+        }
             if (this.pricesByBarcode.has(barcode)) this.display.setText(this.pricesByBarcode.get(barcode));
             else this.display.setText("Product not found for " + barcode);
-        }
     }
 }
