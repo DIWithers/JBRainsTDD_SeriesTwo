a86151a229ab9e8c5429cfa7dd5f6a8e17da7e75
DIWithers <d.ivy.withers@gmail.com>
2017-11-05 11:17:08 -0500

    Removed duplicate test setup

--- spec/pointOfSale.spec.js
+++ spec/pointOfSale.spec.js
@@ -1,30 +1,26 @@
 describe("Point Of Sale Test", function () {
-    it("Product Found", function () {
-        var display = new Display();
-        var sale = new Sale(display, new Map()
+    var display;
+    var sale;
+    beforeEach(function () {
+        display = new Display();
+        sale = new Sale(display, new Map()
             .set("12345", "7.95")
             .set("23456", "12.50"));
+    });
+    it("Product Found", function () {
         sale.onBarcode("12345");
         expect(display.getText()).toBe("7.95");
     });
     it("Another Product Found", function () {
-        var display = new Display();
-        var sale = new Sale(display, new Map()
-            .set("12345", "7.95")
-            .set("23456", "12.50"));
         sale.onBarcode("23456");
         expect(display.getText()).toBe("12.50");
     });
     it("Product not Found", function () {
-        var display = new Display();
-        var sale = new Sale(display, new Map()
-            .set("12345", "7.95")
-            .set("23456", "12.50"));
         sale.onBarcode("99999");
         expect(display.getText()).toBe("Product not found for 99999");
     });
     it("Empty Barcode", function () {
-        var display = new Display();
+        //another form of Refused Bequest - this overrides the setup
         var sale = new Sale(display, null);
         sale.onBarcode("");
         expect(display.getText()).toBe("Scanning error: empty barcode");
--- spec/pointOfSale.spec.ts
+++ spec/pointOfSale.spec.ts
@@ -1,30 +1,26 @@
 describe("Point Of Sale Test", () => {
-    it("Product Found", () => {
-        let display: Display = new Display();
-        let sale: Sale = new Sale(display, new Map()
+    let display: Display;
+    let sale: Sale;
+    beforeEach(() => {
+        display = new Display();
+        sale = new Sale(display, new Map()
             .set("12345", "7.95")
             .set("23456", "12.50"));
+    });
+    it("Product Found", () => {
         sale.onBarcode("12345");
         expect(display.getText()).toBe("7.95");
     });
     it ("Another Product Found", () => {
-        let display: Display = new Display();
-        let sale: Sale = new Sale(display, new Map()
-            .set("12345", "7.95")
-            .set("23456", "12.50"));
         sale.onBarcode("23456");
         expect(display.getText()).toBe("12.50");
     });
     it("Product not Found", () => {
-        let display: Display = new Display();
-        let sale: Sale = new Sale(display, new Map()
-            .set("12345", "7.95")
-            .set("23456", "12.50"));
         sale.onBarcode("99999");
         expect(display.getText()).toBe("Product not found for 99999");
     });
     it("Empty Barcode", () => {
-        let display: Display = new Display();
+        //another form of Refused Bequest - this overrides the setup
         let sale: Sale = new Sale(display, null);
         sale.onBarcode("");
         expect(display.getText()).toBe("Scanning error: empty barcode");
